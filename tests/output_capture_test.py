ae58a2a448ec7a8a7a161a94ac2eaf0d94b155de
Tal Ben-Nun <talbn@llnl.gov>
2024-12-14 19:36:05 -0800

    Add SLURM-based tests

--- tests/output_capture_test.py
+++ tests/output_capture_test.py
@@ -13,7 +13,10 @@
 # SPDX-License-Identifier: (Apache-2.0)
 import os
 import shutil
+import pytest
+
 from hpc_launcher.schedulers.local import LocalScheduler
+from hpc_launcher.schedulers.slurm import SlurmScheduler
 from hpc_launcher.systems import configure
 
 
@@ -43,5 +46,40 @@ def test_output_capture_local():
     shutil.rmtree(launch_dir, ignore_errors=True)
 
 
+@pytest.mark.parametrize('processes', [1, 2])
+def test_output_capture_slurm(processes):
+    if not shutil.which('srun'):
+        pytest.skip('SLURM not available')
+
+    # Configure scheduler
+    system, nodes, procs_per_node = configure.configure_launch(
+        None, 1, processes, None, None)
+    scheduler = SlurmScheduler(nodes, procs_per_node)
+
+    files_before = os.listdir(os.getcwd())
+
+    jobid = scheduler.launch(
+        system, 'python',
+        [os.path.join(os.path.dirname(__file__), 'output_capture.py')])
+    assert jobid is None
+
+    files_after = os.listdir(os.getcwd())
+    new_files = set(files_after) - set(files_before)
+    assert len(new_files) == 1
+
+    launch_dir = os.path.join(os.getcwd(), new_files.pop())
+    assert os.path.isdir(launch_dir)
+    assert os.path.isfile(os.path.join(launch_dir, 'out.log'))
+    assert os.path.isfile(os.path.join(launch_dir, 'err.log'))
+    outfile = open(os.path.join(launch_dir, 'out.log'), 'r').read()
+    errfile = open(os.path.join(launch_dir, 'err.log'), 'r').read()
+    assert outfile.count('output') == processes
+    assert errfile.count('error') == processes
+    shutil.rmtree(launch_dir, ignore_errors=True)
+
+
 if __name__ == '__main__':
     test_output_capture_local()
+    if shutil.which('srun'):
+        test_output_capture_slurm(1)
+        test_output_capture_slurm(2)
