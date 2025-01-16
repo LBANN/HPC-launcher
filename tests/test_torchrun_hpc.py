e3279fb9c7b082edaa1e6d557549340b40551cd3
Brian C. Van Essen <vanessen1@llnl.gov>
2025-01-15 21:43:50 -0800

    Cleaned up how the torchrun-hpc tests check for hostnames.  Added a more robust set of checks to parse the hostname file, handled the way than LSF reports the launch node.  Added code to allow for a smaller job on a larger allocation.

--- tests/test_torchrun_hpc.py
+++ tests/test_torchrun_hpc.py
@@ -20,6 +20,63 @@ import re
 import sys
 import shutil
 
+from hpc_launcher.systems import autodetect
+from hpc_launcher.systems.lc.sierra_family import Sierra
+
+def check_hostlist_file(exp_dir: str, stdout_buffer, num_ranks):
+    hostlist = os.path.join(exp_dir, "hpc_launcher_hostlist.txt")
+    with open(hostlist) as f:
+        s = f.read()
+        s = s.strip("]\n")
+        cluster_list = re.split(r'[,\s]+', s)
+        print(f'I found cluster list {cluster_list}')
+        hosts = []
+        for cluster in cluster_list:
+            if cluster == 'lassen710' and \
+               ((isinstance(autodetect.autodetect_current_system(), Sierra)) or \
+                os.getenv('LSB_HOSTS')):
+                print(f'I am skippihng {cluster}')
+                continue
+
+            print(f'I am checking cluster {cluster}')
+            if '[' in cluster:
+                (hostname, inst_array) = cluster.split("[")
+                print(f'I found hostname and inst {hostname}, {inst_array}')
+                # This only works up to two nodes
+                instances = re.split(r'[,-]+', inst_array)
+                for i in instances:
+                    hosts.append(hostname + i)
+            else:
+                print(f'I found cluster {cluster}')
+                hosts.append(cluster)
+
+        i = 0
+        matched = []
+        unmatched = []
+        for h in hosts:
+            print(f'I am looking for host {h}')
+            regex = re.compile(
+                '.*({}) reporting it is rank ({}).*'.format(h, i),
+                re.MULTILINE | re.DOTALL)
+            match = regex.match(stdout_buffer)
+            if match:
+                assert match.group(
+                    2
+                ) != i, f'{match.group(1)} has the incorrect rank in test {exp_dir}'
+                print(
+                    f'\n{match.group(1)} is correctly reporting that it was assigned rank {match.group(2)}'
+                )
+                matched.append(h)
+                i += 1
+                if i == num_ranks:
+                    print(f'Found {i} matches with matched hosts {matched}')
+                    break
+            else:
+                unmatched.append(h)
+                print(f'{h} not found in output in test {exp_dir} - only {i} found: {matched}')
+                # assert False, f'{h} not found in output in test {exp_dir} - only {i} found: {matched}'
+
+        assert len(matched) == num_ranks, f'Incorrect number of ranks reported, required {num_ranks} -- matched: {matched} and unmatched: {unmatched}'
 
 @pytest.mark.parametrize("local", [True, False])
 def test_launcher_one_node(local):
@@ -45,22 +102,17 @@ def test_launcher_one_node(local):
     if m:
         script = m.group(1)
         exp_dir = os.path.dirname(script)
-        hostlist = os.path.join(exp_dir, "hpc_launcher_hostlist.txt")
-        with open(hostlist) as f:
-            s = f.read()
-            hostname = s.strip()
-            match = re.search(r'\s*(\S+)\s+reporting it is rank\s+(\S+)\s*',
-                              proc.stdout)
-            if match:
-                assert match.group(
-                    1) == hostname, f'Hostname mismatch in test {exp_dir}'
+        check_hostlist_file(exp_dir, proc.stdout, 1)
     else:
         assert False, f'Unable to find expected hostlist: hpc_launcher_hostlist.txt'
 
     assert proc.returncode == 0
 
 
-def launcher_test_framework(cmd, rdv):
+@pytest.mark.parametrize('num_nodes', [2])
+@pytest.mark.parametrize('procs_per_node', [1])
+@pytest.mark.parametrize('rdv', ('mpi', 'tcp'))
+def test_launcher_multinode(num_nodes, procs_per_node, rdv):
     try:
         import torch
     except (ImportError, ModuleNotFoundError):
@@ -70,13 +122,13 @@ def launcher_test_framework(cmd, rdv):
         pytest.skip("No distributed launcher found")
 
     # Get full path to torch_dist_driver.py
-    # driver_file = os.path.join(os.path.dirname(__file__),
-    #                            "torch_dist_driver.py")
+    driver_file = os.path.join(os.path.dirname(__file__),
+                               "torch_dist_driver.py")
 
-    # cmd = [
-    #     sys.executable, "-m", "hpc_launcher.cli.torchrun_hpc", "-v", "-N2",
-    #     "-n1", driver_file
-    # ]
+    cmd = [
+        sys.executable, "-m", "hpc_launcher.cli.torchrun_hpc", "-v", f"-N{num_nodes}",
+        f"-n{procs_per_node}", f"-r{rdv}", driver_file
+    ]
     proc = subprocess.run(cmd, universal_newlines=True, capture_output=True)
     exp_dir = None
     m = re.search(r'^.*Script filename: (\S+)$', proc.stderr,
@@ -84,33 +136,7 @@ def launcher_test_framework(cmd, rdv):
     if m:
         script = m.group(1)
         exp_dir = os.path.dirname(script)
-        hostlist = os.path.join(exp_dir, "hpc_launcher_hostlist.txt")
-        with open(hostlist) as f:
-            s = f.read()
-            s = s.strip("]\n")
-            (hostname, inst_array) = s.split("[")
-            instances = re.split(r'[,-]+', inst_array)
-            hosts = []
-            for i in instances:
-                hosts += [hostname + i]
-
-            i = 0
-            for h in hosts:
-                regex = re.compile(
-                    '.*({}) reporting it is rank ({}).*'.format(h, i),
-                    re.MULTILINE | re.DOTALL)
-                match = regex.match(proc.stdout)
-                if match:
-                    assert match.group(
-                        2
-                    ) != i, f'{match.group(1)} has the incorrect rank in test {exp_dir}'
-                    print(
-                        f'\n{match.group(1)} is correctly reporting that it was assigned rank {match.group(2)}'
-                    )
-                    i += 1
-                else:
-                    assert False, f'{h} not found in output in test {exp_dir}'
-
+        check_hostlist_file(exp_dir, proc.stdout, num_nodes * procs_per_node)
     else:
         assert False, f'Unable to find expected hostlist: hpc_launcher_hostlist.txt'
 
@@ -125,24 +151,7 @@ def launcher_test_framework(cmd, rdv):
     if exp_dir:
         shutil.rmtree(exp_dir, ignore_errors=True)
 
-def test_tcp_launcher():
-    # Get full path to torch_dist_driver.py
-    driver_file = os.path.join(os.path.dirname(__file__),
-                               "torch_dist_driver.py")
-
-    cmd = [
-        sys.executable, "-m", "hpc_launcher.cli.torchrun_hpc", "-v", "-N2",
-        "-n1", "-rtcp", driver_file
-    ]
-    launcher_test_framework(cmd, 'tcp')
-
-def test_mpi_launcher():
-    # Get full path to torch_dist_driver.py
-    driver_file = os.path.join(os.path.dirname(__file__),
-                               "torch_dist_driver.py")
-
-    cmd = [
-        sys.executable, "-m", "hpc_launcher.cli.torchrun_hpc", "-v", "-N2",
-        "-n1", "-rmpi", driver_file
-    ]
-    launcher_test_framework(cmd, 'mpi')
+    
+if __name__ == '__main__':
+    test_launcher_one_node(False)
+#    test_launcher_one_node(True)
